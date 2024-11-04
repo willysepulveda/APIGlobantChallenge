06d5babecff6b904b49e03e30f952ce5be9242dc
Willy <willynodier19@hotmail.com>
2024-11-04 18:36:53 -0500

    Create startup.sh file

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+apt-get install -y unixodbc
+apt-get install -y curl
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
